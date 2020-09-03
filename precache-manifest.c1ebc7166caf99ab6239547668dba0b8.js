fd39b4d7878124098ed6c4de6886ab44f303fc7c
Gabriel <57782295+Gabriel-demian@users.noreply.github.com>
2020-09-03 02:47:04 -0300

    Updates

---
+++ precache-manifest.c1ebc7166caf99ab6239547668dba0b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095c94805fd4661f526a10237d7fdeb2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5d526d13643ea8f7728f",
+    "url": "/monsters-rolodex/static/css/main.73a7ab34.chunk.css"
+  },
+  {
+    "revision": "5db257ba903a8fee2e3c",
+    "url": "/monsters-rolodex/static/js/2.c75ba697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.c75ba697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d526d13643ea8f7728f",
+    "url": "/monsters-rolodex/static/js/main.c7d49e80.chunk.js"
+  },
+  {
+    "revision": "0bd73d68cf04fc8e9a1d",
+    "url": "/monsters-rolodex/static/js/runtime-main.b775fccc.js"
+  }
+]);
